6be863b57f6271efb1b554584a186335b59b5068
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-07-11 13:34:51 +0530

    arranging the spaces b/w variables

--- Assignment01/Assignment_01.py
+++ Assignment01/Assignment_01.py
@@ -7,47 +7,45 @@ data = pd.read_csv('headbrain.csv')
 print(data.head())
 
 # collecting X and Y
-X=data['Head Size(cm^3)'].values
-Y=data['Brain Weight(grams)'].values
+X = data['Head Size(cm^3)'].values
+Y = data['Brain Weight(grams)'].values
 
 # calculating coefficient
 
 # mean X and Y
-mean_x=np.mean(X)
-mean_y=np.mean(Y)
+mean_x = np.mean(X)
+mean_y = np.mean(Y)
 print(mean_x)
 print(mean_y)
 
-
 # total number of values
-n=len(X)
+n = len(X)
 
 # using the formula to calculate b0 and b1
-numer=0
-denom=0
+numer = 0
+denom = 0
 
 for i in range(n):
-    numer+=(X[i]-mean_x)*(Y[i]-mean_y)
-    denom+=(X[i]-mean_x)**2
-b1=numer/denom
-b0=mean_y-(b1*mean_x)
-
+    numer += (X[i] - mean_x) * (Y[i] - mean_y)
+    denom += (X[i] - mean_x) ** 2
+b1 = numer / denom
+b0 = mean_y - (b1 * mean_x)
 
 # printing coefficient
 print(b1)
 print(b0)
 
-max_x=np.max(X)+100
-min_x=np.min(X)+100
+max_x = np.max(X) + 100
+min_x = np.min(X) + 100
 
 # calculating line values x and y
-x=np.linspace(min_x,max_x,1000)
-y=b0+b1*x
+x = np.linspace(min_x, max_x, 1000)
+y = b0 + b1 * x
 
 # ploting line
-plt.plot(x,y,color='#58b970',label='Regression Line')
+plt.plot(x, y, color='#58b970', label='Regression Line')
 # ploting Scatter Points
-plt.scatter(X,Y,c='#ef5423',label='Scatter Plot')
+plt.scatter(X, Y, c='#ef5423', label='Scatter Plot')
 
 plt.xlabel('Head Size(cm^3)')
 plt.ylabel('Brain Weight(grams)')
@@ -55,20 +53,20 @@ plt.legend()
 plt.show()
 
 # calculating Root Mean Squares Error
-rmse=0
+rmse = 0
 for i in range(n):
-    y_pred=b0+b1*X[i]
-    rmse+=(Y[i]-y_pred)**2
-rmse=np.sqrt(rmse/n)
+    y_pred = b0 + b1 * X[i]
+    rmse += (Y[i] - y_pred) ** 2
+rmse = np.sqrt(rmse / n)
 print("RMSE")
 print(rmse)
 # calculating R2 Score
-ss_tot=0
-ss_res=0
+ss_tot = 0
+ss_res = 0
 for i in range(n):
-    y_pred=b0+b1*X[i]
-    ss_tot+=(Y[i]-mean_y)**2
-    ss_res+=(Y[i]-y_pred)**2
-r2=1-(ss_res/ss_tot)
+    y_pred = b0 + b1 * X[i]
+    ss_tot += (Y[i] - mean_y) ** 2
+    ss_res += (Y[i] - y_pred) ** 2
+r2 = 1 - (ss_res / ss_tot)
 print("R2 Score")
 print(r2)
