29ecfc2ef3e8e43f92d591b7ccd48d5abc8e33ff
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-07-11 13:09:14 +0530

    separate data into X and Y

--- Assignment01/Assignment_01.py
+++ Assignment01/Assignment_01.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 # reading Data
 data = pd.read_csv('headbrain.csv')
 print(data.head())
+
+# collecting X and Y
+X=data['Head Size(cm^3)'].values
+Y=data['Brain Weight(grams)'].values
+
+# calculating coefficient
+
+# mean X and Y
