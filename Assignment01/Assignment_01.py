90a0b5fba3f31b4b2d916b76708c0e7b217f4f57
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-07-11 13:12:10 +0530

    calculating total number of columns in the data

--- Assignment01/Assignment_01.py
+++ Assignment01/Assignment_01.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -12,3 +13,13 @@ Y=data['Brain Weight(grams)'].values
 # calculating coefficient
 
 # mean X and Y
+mean_x=np.mean(X)
+mean_y=np.mean(Y)
+print(mean_x)
+print(mean_y)
+
+
+# total number of values
+n=len(X)
+
+# using the formula to calculate b1 and b2
